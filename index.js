3a060c0026fb6185a7de055d1509166a416653ce
Emmanuel Benjamin D <emmanuel26112000@gmail.com>
2024-04-22 17:44:10 +0530

    slash removed updated

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 app.all("/*", function (req, res, next) {
   
-  res.header("Access-Control-Allow-Origin", "https://e-shopper24.netlify.app/", "http://localhost:5173/");
+  res.header("Access-Control-Allow-Origin", "https://e-shopper24.netlify.app", "http://localhost:5173");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   res.header(`Access-Control-Allow-Headers`, `Content-Type`);
